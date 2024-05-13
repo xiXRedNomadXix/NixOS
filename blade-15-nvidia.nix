40ebebb883b50fa0d038846317b0d33da12d8496
xiXRedNomadXix <124952504+xiXRedNomadXix@users.noreply.github.com>
2024-05-12 20:53:27 -0400

    Update blade-15-nvidia.nix

--- blade-15-nvidia.nix
+++ blade-15-nvidia.nix
@@ -66,6 +66,15 @@
   };
   ...
 
+{
+  hardware.nvidia.prime = {
+    sync.enable = true;
+
+    # Make sure to use the correct Bus ID values for your system!
+    nvidiaBusId = "PCI:14:0:0";
+    intelBusId = "PCI:0:2:0";
+  };
+}
 
 
 
